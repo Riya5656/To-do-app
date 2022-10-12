71428fb238d87ad8736510dc87890444f526ca4c
Riya5656 <riya.parajuli@gmail.com>
2022-10-12 21:11:51 +0545

    add,delete and update

---
+++ Resource/todo.js
@@ -0,0 +1,25 @@
+const todos=[
+    {
+        "id":"001",
+        "title":"complete bootcamp project",
+        "description":"Talk to team,make team repository,assign task",
+    },
+    {
+        "id":"002",
+        "title":"Go to market",
+        "description":"Buy vegetables, buy clothes, buy water bottle"
+    },
+
+    {
+        "id":"003",
+        "title":"Cooking",
+        "description":"Cook food for family of 5 people"
+    },
+
+    {
+        "id":"004",
+        "title":"Take medicines",
+        "description":"Take 3 pills 3 times a day in differance of 8 hours"
+    }
+]
+module.exports=todos;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var todos=require('../Resource/todo');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { fname: 'Riya ', lname:'Parajuli',age:22 });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { fname: 'Riya ', lname:'Parajuli',age:22 });
+// });
+
+router.get('/',function(req,res,next){
+res.render('home',{todoslist:todos});
 });
 
-router.get('/home',function(req,res,next){
-res.render('home')
+router.get('/add-to-do',function(req,res,next){
+  res.render('addToDo',{Title:'Add ToDo'});
+  });
+
+  router.post('/save-to-do', function(req,res,next){
+    todos.push({...req.body,_id:`00${todos.length}`}); //... (spread operator)
+    res.redirect('/');
+  })
+router.get ('/delete-to-do/:index',function(req,res,next){
+console.log(req.params.index)
+todos.splice(req.params.index, 1);
+res.redirect('/');
+
 })
+
+router.get('/open-update-form/:id',function(req,res,next){
+  const todo1 =todos.find(todo=> todo.id === req.params.id)
+  res.render('editToDo',{todo:todo1});
+})
+
+router.post('/update-to-do/:id',function(req,res,next){
+  const index=todos.findIndex(todo=>todo.id === req.params.id);
+  todos.splice(index,1,{...req.body,id: req.params.id});
+  res.redirect('/');
+})
+
 module.exports = router;
+
